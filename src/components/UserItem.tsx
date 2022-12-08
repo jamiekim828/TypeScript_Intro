2a350862eec13055fdfb6c59243b31554854941b
jamiekim828 <jamiekim828@gmail.com>
2022-12-08 17:33:04 +0100

    render the user data

--- src/components/UserItem.tsx
+++ src/components/UserItem.tsx
@@ -1,5 +1,45 @@
-import React from "react";
+import React from 'react';
 
-export default function UserItem() {
-  return <div>UserItem</div>;
+type UserType = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipCode: string;
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
+
+export default function UserItem({ user }: { user: UserType }) {
+  return (
+    <div className='user'>
+      <img
+        src='https://freesvg.org/img/abstract-user-flat-4.png'
+        alt='profile-img'
+        className='profile-img'
+      />
+      <p>Name: {user.name}</p>
+      <p>User name: {user.username}</p>
+      <p>Phone: {user.phone}</p>
+      <div className='address'>
+        Address:{' '}
+        <ul>
+          <li>street: {user.address.street}</li>
+          <li>suite: {user.address.suite}</li>
+          <li>city: {user.address.city}</li>
+          <li>zipcode: {user.address.zipCode}</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -1,5 +1,37 @@
-import React from "react";
+import React from 'react';
+import UserItem from './UserItem';
 
-export default function UserList() {
-  return <div>User</div>;
+type UserType = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipCode: string;
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+};
+
+export default function UserList({ user }: { user: UserType[] }) {
+  return (
+    <div className='user-list-container'>
+      <div className='header'>
+        <h1>User List</h1>
+      </div>
+      <div className='user-wrapper'>
+        {user.map((user) => (
+          <UserItem user={user} />
+        ))}
+      </div>
+    </div>
+  );
 }
